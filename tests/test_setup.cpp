e4c3642b4387aed9eabc32aaceba929daebb57ac
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2026-07-05 19:15:39 +0100

    :sparkles: add ability to control level of logs in the tests using --log

--- tests/test_setup.cpp
+++ tests/test_setup.cpp
@@ -1,18 +1,35 @@
 #include <gtest/gtest.h>
+#include <spdlog/common.h>
 #include <spdlog/spdlog.h>
 
-namespace {
-
 class SpdlogEnv : public ::testing::Environment {
+    spdlog::level::level_enum m_level = spdlog::level::info;
+
   public:
+    void set_level(spdlog::level::level_enum level) { m_level = level; }
+
     void SetUp() override
     {
-        spdlog::set_level(spdlog::level::debug);
+        spdlog::set_level(m_level);
         spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [thread %t] %v");
+
+        SPDLOG_DEBUG("SpdlogEnv::SetUp() - spdlog initialized");
     }
 };
 
-const auto* const g_spdlog_env = ::testing::AddGlobalTestEnvironment(
-    new SpdlogEnv);
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
 
-}  // namespace
+    SpdlogEnv* logger = new SpdlogEnv;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg.starts_with("--log=")) {
+            logger->set_level(spdlog::level::from_str(arg.substr(6)));
+        }
+    }
+
+    ::testing::AddGlobalTestEnvironment(logger);
+    return RUN_ALL_TESTS();
+}
